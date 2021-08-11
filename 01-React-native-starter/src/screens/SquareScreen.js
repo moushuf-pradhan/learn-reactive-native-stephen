34c4b0c30ee9fc77208f32f0eae315ea6f9f2676
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-12 02:28:09 +0545

    49. App overview

---
+++ 01-React-native-starter/src/screens/SquareScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const SquareScreen = function () {
+	return <Text>Square screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
