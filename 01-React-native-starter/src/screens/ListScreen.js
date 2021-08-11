291d2fab7310d6129a8ed0a015a643fbde51d0b1
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-11 15:36:18 +0545

    21. Rendering a FlatList

--- 01-React-native-starter/src/screens/ListScreen.js
+++ 01-React-native-starter/src/screens/ListScreen.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = function () {
-	return <Text>List screen</Text>;
+	const friends = [
+		{
+			name: 'Friend #1',
+		},
+		{
+			name: 'Friend #2',
+		},
+		{
+			name: 'Friend #3',
+		},
+		{
+			name: 'Friend #4',
+		},
+		{
+			name: 'Friend #5',
+		},
+		{
+			name: 'Friend #6',
+		},
+		{
+			name: 'Friend #7',
+		},
+		{
+			name: 'Friend #8',
+		},
+		{
+			name: 'Friend #9',
+		},
+	];
+	return (
+		<FlatList
+			data={friends}
+			renderItem={({ item }) => <Text>{item.name}</Text>}
+		></FlatList>
+	);
 };
 
 const styles = StyleSheet.create({});
