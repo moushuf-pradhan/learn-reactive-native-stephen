1d2bfe3df305f53a98321bad5b0f9bffe962b67e
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-11 16:11:06 +0545

    28. Buttons in action

--- 01-React-native-starter/src/screens/HomeScreen.js
+++ 01-React-native-starter/src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+	return (
+		<View>
+			<Text style={styles.text}>HomeScreen</Text>
+			<Button
+				title="Go to Components Demo"
+				onPress={() => console.log('Button pressed')}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+	text: {
+		fontSize: 30,
+	},
 });
 
 export default HomeScreen;
