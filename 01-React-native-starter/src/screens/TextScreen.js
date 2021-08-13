431e1afe6039d627d817884764fce5820ab7b195
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-13 16:31:32 +0545

    65. Two important props

--- 01-React-native-starter/src/screens/TextScreen.js
+++ 01-React-native-starter/src/screens/TextScreen.js
@@ -4,7 +4,11 @@ import { Text, View, StyleSheet, TextInput } from 'react-native';
 export const TextScreen = function () {
 	return (
 		<View>
-			<TextInput style={styles.input} />
+			<TextInput
+				style={styles.input}
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
 		</View>
 	);
 };
