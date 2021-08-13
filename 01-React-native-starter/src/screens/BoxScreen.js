39ee568102a3b2fb4c0879bcb6aedf49c2a9c372
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-13 17:01:08 +0545

    70. Layout with React native

---
+++ 01-React-native-starter/src/screens/BoxScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+
+export const BoxScreen = function () {
+	return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
