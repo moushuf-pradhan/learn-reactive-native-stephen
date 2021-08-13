28bac55692596b0c28e269ea7b3d6a8004e27bbb
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-13 16:21:12 +0545

    63. Handling text input

---
+++ 01-React-native-starter/src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export const TextScreen = function () {
+	return <Text>Text Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
