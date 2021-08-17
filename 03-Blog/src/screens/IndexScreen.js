afaec92166350cf25b94fae37a9b83f5d5de32ea
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-17 17:57:45 +0545

    134. It works !

--- 03-Blog/src/screens/IndexScreen.js
+++ 03-Blog/src/screens/IndexScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 export default function IndexScreen() {
+	const { data, addBlogPost } = React.useContext(BlogContext);
 	return (
 		<View>
+			<Button title="Add post" onPress={addBlogPost} />
 			<FlatList
-				data={blogPosts}
+				data={data}
 				keyExtractor={post => post.title}
 				renderItem={({ item }) => <Text>{item.title}</Text>}
 			/>
