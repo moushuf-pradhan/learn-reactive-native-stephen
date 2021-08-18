c6c52c7dad66af5bce4dcc775d9ea7e5b41c60a6
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-18 22:16:01 +0545

    147. Displaying a Form

--- 03-Blog/src/screens/CreateScreen.js
+++ 03-Blog/src/screens/CreateScreen.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function CreateScreen() {
+	// Local states
+	const [title, setTitle] = React.useState('');
+	const [content, setContent] = React.useState('');
 	return (
 		<View>
-			<Text></Text>
+			<Text style={styles.label}>Enter title:</Text>
+			<TextInput
+				style={styles.input}
+				value={title}
+				onChangeText={val => setTitle(val)}
+			/>
+			<Text style={styles.label}>Enter content:</Text>
+			<TextInput
+				style={styles.input}
+				value={content}
+				onChangeText={val => setContent(val)}
+			/>
+			<Button title="Add a blog post" />
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	input: {
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: 'black',
+		margin: 5,
+		marginBottom: 15,
+		padding: 5,
+	},
+	label: {
+		fontSize: 20,
+		marginBottom: 5,
+		marginLeft: 5,
+	},
+});
