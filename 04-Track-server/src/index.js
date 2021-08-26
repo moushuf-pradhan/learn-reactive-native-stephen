81ee6571561609c467f5bde84ec9fbd3f4d1a1cf
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-26 18:43:18 +0545

    176. Connecting to MongoDB

--- 04-Track-server/src/index.js
+++ 04-Track-server/src/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Create an app object
 const app = express();
+
+// Mongo URI
+const mongoURI =
+	'mongodb+srv://admin:admin@react-native-tracker-wi.hnbjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+// Connect to Mongo instance
+mongoose.connect(mongoURI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+
+// On Mongo connection success
+mongoose.connection.on('connected', () => {
+	console.log('Connected to mongo instance');
+});
+// On Mongo connection failure
+mongoose.connection.on('error', err => {
+	console.error('Failed to connect to mongo instance', err);
+});
 
 app.get('/', (req, res) => {
 	res.send('Hi there !');
