ed4bd6f60dada4a0e9075a7781d02a6ff369fb89
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-08-30 21:54:16 +0545

    188. Salting and Hashing

--- 04-Track-server/src/models/user.js
+++ 04-Track-server/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Create user Schema
 const userSchema = new mongoose.Schema({
@@ -12,5 +13,37 @@ const userSchema = new mongoose.Schema({
 		required: true,
 	},
 });
+
+userSchema.pre('save', function () {
+	const user = this;
+	if (!user.isModified('password')) return next();
+
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, function (err, hash) {
+			if (err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+	const user = this;
+	return new Promise(function (resolve, reject) {
+		bcrypt.compare(
+			candidatePassword,
+			user.password,
+			function (err, isMatch) {
+				if (err) return reject(err);
+
+				if (!isMatch) return reject(false);
+
+				resolve(true);
+			}
+		);
+	});
+};
 
 mongoose.model('User', userSchema);
