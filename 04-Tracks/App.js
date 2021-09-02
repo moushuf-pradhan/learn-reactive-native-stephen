cf94b62f2ec118b03bc2fcbfa7674eac4043d4df
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-09-02 17:54:52 +0545

    207. The auth context

--- 04-Tracks/App.js
+++ 04-Tracks/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+// Import Provider
+import { Provider as AuthProvider } from './src/context/AuthContext';
+// Import components
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -26,4 +28,12 @@ const switchNavigator = createSwitchNavigator({
 	}),
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+	return (
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	);
+};
