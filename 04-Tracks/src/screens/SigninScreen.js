86d77ed979ae1cd1505d57d632e8ec89255923e5
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-09-03 21:30:10 +0545

    222. Real component reuse

--- 04-Tracks/src/screens/SigninScreen.js
+++ 04-Tracks/src/screens/SigninScreen.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// Import components
+import AuthForm from '../components/AuthForm';
+// Import context
+import { Context as AuthContext } from '../context/AuthContext';
 
-export default function SigninScreen() {
+export default function SigninScreen(props) {
+	// Destructuring Context
+	const { state, signin } = React.useContext(AuthContext);
 	return (
-		<View>
-			<Text></Text>
-		</View>
+		<AuthForm
+			heading="Sign in to your account"
+			errorMessage={state.errorMessage}
+			submitBtnText="Sign in"
+			onSubmit={signin}
+			helperText="Don't have an account? Sign up instead."
+			onHelperTextClick={() => props.navigation.navigate('Signup')}
+		/>
 	);
 }
+
+// Hide screen header
+SigninScreen.navigationOptions = () => {
+	return {
+		headerShown: false,
+	};
+};
 
 const styles = StyleSheet.create({});
