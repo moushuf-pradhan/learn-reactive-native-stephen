295109cd55d4600ac17197644357544d8b5a82c0
Moushuf Pradhan <moushuf.pradhan@gmail.com>
2021-09-03 15:42:58 +0545

    218. Extracting Form logic

---
+++ 04-Tracks/src/components/AuthForm.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+// Import from React native elements
+import { Text, Input, Button } from 'react-native-elements';
+// Import components
+import Spacer from '../components/Spacer';
+
+export default function AuthForm(props) {
+	// Local states
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	return (
+		<View style={styles.container}>
+			<Spacer>
+				<Text h3 style={styles.authHeading}>
+					{props.heading}
+				</Text>
+			</Spacer>
+			<Input
+				label="Email"
+				value={email}
+				onChangeText={setEmail}
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
+			<Spacer />
+			<Input
+				secureTextEntry
+				label="Password"
+				value={password}
+				onChangeText={setPassword}
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
+			{props.errorMessage ? (
+				<Text style={styles.errorMessage}>{props.errorMessage}</Text>
+			) : null}
+			<Spacer>
+				<Button
+					title={props.submitBtnText}
+					onPress={() => props.onSubmit({ email, password })}
+				/>
+			</Spacer>
+			<Spacer />
+			<TouchableOpacity onPress={props.onHelperTextClick}>
+				<Spacer>
+					<Text style={styles.link}>{props.helperText}</Text>
+				</Spacer>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: 75,
+		marginBottom: 150,
+	},
+	authHeading: {
+		marginBottom: 40,
+	},
+	errorMessage: {
+		fontSize: 16,
+		color: 'red',
+		marginHorizontal: 10,
+		marginBottom: 10,
+	},
+	link: {
+		color: 'blue',
+	},
+});
--- 04-Tracks/src/screens/SignupScreen.js
+++ 04-Tracks/src/screens/SignupScreen.js
@@ -1,63 +1,22 @@
 import React from 'react';
 // Import from React native
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-// Import from React native elements
-import { Text, Input, Button } from 'react-native-elements';
-// Import components
-import Spacer from '../components/Spacer';
+import { StyleSheet } from 'react-native';
+import AuthForm from '../components/AuthForm';
 // Import context
 import { Context as AuthContext } from '../context/AuthContext';
 
 export default function SignupScreen(props) {
-	// Local states
-	const [email, setEmail] = React.useState('');
-	const [password, setPassword] = React.useState('');
-
 	// Destructuring AuthContext
 	const { state, signup } = React.useContext(AuthContext);
 	return (
-		<View style={styles.container}>
-			<Spacer>
-				<Text h3 style={styles.authHeading}>
-					Sign up for Tracker
-				</Text>
-			</Spacer>
-			<Input
-				label="Email"
-				value={email}
-				onChangeText={setEmail}
-				autoCapitalize="none"
-				autoCorrect={false}
-			/>
-			<Spacer />
-			<Input
-				secureTextEntry
-				label="Password"
-				value={password}
-				onChangeText={setPassword}
-				autoCapitalize="none"
-				autoCorrect={false}
-			/>
-			{state.errorMessage ? (
-				<Text style={styles.errorMessage}>{state.errorMessage}</Text>
-			) : null}
-			<Spacer>
-				<Button
-					title="Sign up"
-					onPress={() => signup({ email, password })}
-				/>
-			</Spacer>
-			<Spacer />
-			<TouchableOpacity
-				onPress={() => props.navigation.navigate('Signin')}
-			>
-				<Spacer>
-					<Text style={styles.link}>
-						Already have an account? Sign in instead.
-					</Text>
-				</Spacer>
-			</TouchableOpacity>
-		</View>
+		<AuthForm
+			heading="Sign up for Tracker"
+			errorMessage={state.errorMessage}
+			submitBtnText="Sign up"
+			onSubmit={signup}
+			helperText="Already have an account? Sign in instead."
+			onHelperTextClick={() => props.navigation.navigate('Signin')}
+		/>
 	);
 }
 
@@ -68,21 +27,4 @@ SignupScreen.navigationOptions = () => {
 	};
 };
 
-const styles = StyleSheet.create({
-	container: {
-		marginTop: 75,
-		marginBottom: 150,
-	},
-	authHeading: {
-		marginBottom: 40,
-	},
-	errorMessage: {
-		fontSize: 16,
-		color: 'red',
-		marginHorizontal: 10,
-		marginBottom: 10,
-	},
-	link: {
-		color: 'blue',
-	},
-});
+const styles = StyleSheet.create({});
